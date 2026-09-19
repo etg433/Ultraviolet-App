d4e01b483843d10122edf24e1c86199085686f02
etg433 <eric@njmac.me>
2026-09-19 10:21:00 -0400

    Build CreatorForge storefront foundation

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import { createServer } from "node:http";
 
 const app = express();
 const port = Number(process.env.PORT) || 8080;
+
 app.use(express.static("./public"));
-app.get("*", (req, res) => res.sendFile("index.html", { root: "./public" }));
+app.use((req, res) => res.sendFile("index.html", { root: "./public" }));
+
 const server = createServer(app);
-server.listen(port, "0.0.0.0", () => console.log(`CreatorForge listening on port ${port}`));
-function shutdown(){ server.close(() => process.exit(0)); }
+server.listen(port, "0.0.0.0", () => {
+  console.log(`CreatorForge listening on port ${port}`);
+});
+
+function shutdown() {
+  server.close(() => process.exit(0));
+}
+
 process.on("SIGINT", shutdown);
 process.on("SIGTERM", shutdown);
